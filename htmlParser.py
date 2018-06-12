83f7a722dae5776715713adc92d060261d902caa
David Martin <dmartin@grovesindustrial.com>
2018-06-12 09:20:28 -0500

    adding openTag function and closeTag function and added feed from index.php

--- htmlParser.py
+++ htmlParser.py
@@ -1,11 +1,98 @@
 from html.parser import HTMLParser
 from html.entities import name2codepoint
 
+def openTag(tag,attr):
+    sAttr = '['
+    for attr in attrs:
+        sAttr += attr + ','
+    if(tag == 'html'):
+        string = "$this->openHtml(FALSE);"
+    if(tag == 'title')
+        string = "$this->Title( {%s}, FALSE );" sAttr
+    if(tag == 'span')
+        string = "$this->openSpan( {%s}, FALSE );" sAttr
+    if(tag == 'head')
+        string = "$this->openHead( {%s}, FALSE );" sAttr
+    if(tag == 'body')
+        string = "$this->openBody( {%s}, FALSE );" sAttr
+    if(tag == 'p')
+        string = "$this->openP( {%s}, FALSE );" sAttr
+    if(tag == 'noscript')
+        string = "$this->NoScript( {%s}, FALSE );" sAttr
+    if(tag == 'link')
+        string = "$this->Link( {%s}, FALSE );" sAttr
+    if(tag == 'button')
+        string = "$this->Button( {%s}, FALSE );" sAttr
+    if(tag == 'iframe')
+        string = "$this->iFrame( {%s}, FALSE );" sAttr
+    if(tag == 'meta')
+        string = "$this->Meta( {%s}, FALSE );" sAttr
+    if(tag == 'script')
+        string = "$this->Script( {%s}, FALSE );" sAttr
+    if(tag == 'div')
+        string = "$this->openDiv( {%s}, FALSE );" sAttr
+    if(tag == 'label')
+        string = "$this->Label( {%s}, FALSE );" sAttr
+    if(tag == 'input')
+        string = "$this->Input( {%s}, FALSE );" sAttr
+
+        
+
+def closeTag(tag,attr):
+    sAttr = '['
+    for attr in attrs:
+        sAttr += attr + ','
+    if(tag == 'html'):
+        string = "$this->closeHtml(FALSE);"
+    if(tag == 'title')
+        string = "$this->Title( {%s}, FALSE );" sAttr
+    if(tag == 'span')
+        string = "$this->closeSpan( {%s}, FALSE );" sAttr
+    if(tag == 'head')
+        string = "$this->closeHead( {%s}, FALSE );" sAttr
+    if(tag == 'body')
+        string = "$this->closeBody( {%s}, FALSE );" sAttr
+    if(tag == 'p')
+        string = "$this->closeP( {%s}, FALSE );" sAttr
+    if(tag == 'noscript')
+        string = "$this->NoScript( {%s}, FALSE );" sAttr
+    if(tag == 'link')
+        string = "$this->Link( {%s}, FALSE );" sAttr
+    if(tag == 'button')
+        string = "$this->Button( {%s}, FALSE );" sAttr
+    if(tag == 'iframe')
+        string = "$this->iFrame( {%s}, FALSE );" sAttr
+    if(tag == 'meta')
+        string = "$this->Meta( {%s}, FALSE );" sAttr
+    if(tag == 'script')
+        string = "$this->Script( {%s}, FALSE );" sAttr
+    if(tag == 'div')
+        string = "$this->closeDiv( {%s}, FALSE );" sAttr
+    if(tag == 'label')
+        string = "$this->Label( {%s}, FALSE );" sAttr
+    if(tag == 'input')
+        string = "$this->Input( {%s}, FALSE );" sAttr
+
+    
+    
+    
+    
+    
+    
+        
+    
+    
+        
+    
+        
+    
+
 class MyHtmlParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         print("Start tag: ", tag)
         for attr in attrs:
             print("    attr:", attr)
+        createTag(tag,attr);
     def handle_endtag(self, tag):
         print("End tag   :", tag)
     def handle_data(self, data):
@@ -26,5 +113,5 @@ class MyHtmlParser(HTMLParser):
 
 
 parser = MyHtmlParser();
-
-parser.feed('<html><head><title>My Foot</title></head><body><p>My Foot</p></body></html>');
+str = open('index.html', 'r').read()
+parser.feed(str);
