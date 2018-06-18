cfdc43f85ec9387de882d975f106384ec934778f
David Martin <dmartin@grovesindustrial.com>
2018-06-18 08:20:32 -0500

    fixes
    1.) fixes strings
    2.) create fAttr string
    3.)  added convertTuple to convert array to string
    4.)  removed { }
    5.)  added some logic to remove close tags when they atre not needed and othetr minor fixes

--- htmlParser.py
+++ htmlParser.py
@@ -1,125 +1,179 @@
 from html.parser import HTMLParser
 from html.entities import name2codepoint
 
-strings = ["" for x in range(500)]
+strings = []
 
-def convertTuple(aInput):
+fAttr= ""
+
+
+def convertTuple(aInput, data=None):
     str = '['
+    if data != None and data != "":
+        temp = ['text', data] 
+        aInput.append(data)
+    length = len(aInput)
+    counter = 0
     for attr in aInput:
-        str = str + attr[0] + '=>' + attr[1] + ', '
-    str = ']'
+        if len(attr[0]) == 1 and len(attr[1]) == 1:
+            if data != None:
+                str += "'text' =>" + "'" + data + "'"
+        elif counter < (length-1):
+            str += '\''+attr[0] +'\'' + '=>' + '\'' + attr[1] + '\'' +', '
+        else:
+            str += '\''+attr[0] +'\'' + '=>' + '\'' + attr[1] + '\''
+            if data != None:
+                str += ', ' + "'text' =>" + "'" + data + "'"
+        counter += 1
+    str += ']'
     return str
+
+
+
+   
         
 
 def openTag(tag,attrs):
     
     if tag == 'html':
         strings.append(  "$this->openHtml(FALSE);")
-    if tag == 'title':
-        stri =  "$this->Title( {%s}, FALSE );" % convertTuple(attrs)
+    elif tag == 'title':
+        fAttr = attrs
+    elif tag == 'span':
+        strings.append(  "$this->openSpan( %s, FALSE );" % convertTuple(attrs))
+    elif tag == 'head':
+        stri = "$this->openHead( %s, FALSE );" % convertTuple(attrs)
         strings.append( stri )
-    if tag == 'span':
-        strings.append(  "$this->openSpan( {%s}, FALSE );" % convertTuple(attrs))
-    if tag == 'head':
-        stri = "$this->openHead( {%s}, FALSE );" % convertTuple(attrs)
-        strings.append( stri )
-    if tag == 'body':
-        stri = "$this->openBody( {%s}, FALSE );" % convertTuple(attrs)
+    elif tag == 'body':
+        stri = "$this->openBody( %s, FALSE );" % convertTuple(attrs)
         strings.append(stri)
-    if tag == 'p':
-        stri = "$this->openP( {%s}, FALSE );" % convertTuple(attrs)
+    elif tag == 'p':
+        stri = "$this->openP( %s, FALSE );" % convertTuple(attrs)
         strings.append( stri )
-    if tag == 'noscript':
-        stri = "$this->NoScript( {%s}, FALSE );" % convertTuple(attrs)
+    elif tag == 'noscript':
+        stri = "$this->NoScript( %s, FALSE );" % convertTuple(attrs)
         strings.append( stri )
-    if tag == 'link':
-        stri = "$this->Link( {%s}, FALSE );" % convertTuple(attrs)
+    elif tag == 'link':
+        stri = "$this->Link( %s, FALSE );" % convertTuple(attrs)
         strings.append( stri )
-    if tag == 'button':
-        stri = "$this->Button( {%s}, FALSE );" % convertTuple(attrs)
+    elif tag == 'button':
+        stri = "$this->Button( %s, FALSE );" % convertTuple(attrs)
         strings.append( stri  )
-    if tag == 'iframe':
-        stri = "$this->iFrame( {%s}, FALSE );" % convertTuple(attrs)
+    elif tag == 'iframe':
+        stri = "$this->iFrame( %s, FALSE );" % convertTuple(attrs)
         strings.append(stri  )
-    if tag == 'meta':
-        stri = "$this->Meta( {%s}, FALSE );" % convertTuple(attrs)
+    elif tag == 'meta':
+        stri = "$this->Meta( %s, FALSE );" % convertTuple(attrs)
         strings.append( stri )
-    if tag == 'script':
-        stri = "$this->Script( {%s}, FALSE );" % convertTuple(attrs)
+    elif tag == 'script':
+        stri = "$this->Script( %s, FALSE );" % convertTuple(attrs)
         strings.append( stri )
-    if tag == 'div':
-        stri = "$this->openDiv( {%s}, FALSE );" % convertTuple(attrs)
+    elif tag == 'div':
+        stri = "$this->openDiv( %s, FALSE );" % convertTuple(attrs)
         strings.append( stri )
-    if tag == 'label':
-        stri = "$this->Label( {%s}, FALSE );" % convertTuple(attrs)
+    elif tag == 'label':
+        stri = "$this->Label( %s, FALSE );" % convertTuple(attrs)
         strings.append( stri )
-    if tag == 'input':
-        stri = "$this->Input( {%s}, FALSE );" % convertTuple(attrs)
+    elif tag == 'input':
+        stri = "$this->Input( %s, FALSE );" % convertTuple(attrs)
         strings.append( stri  )
+    elif tag == 'form':
+        stri = "$this->openForm( %s, FALSE );" % convertTuple(attrs)
+        strings.append( stri  )
+    elif tag == 'a':
+        stri = "$this->ATag( %s, FALSE );" % convertTuple(attrs)
+        strings.append( stri  )
+    elif tag == 'img':
+        stri = "$this->Image( %s, FALSE );" % convertTuple(attrs)
+        strings.append( stri  )
+    else:
+        strings.append("echo 'Error: no matching code for tag: %s'" % tag)
 
         
 
-def closeTag(tag):
-    sAttr = '['
-    
+def closeTag(tag, attr=None, data=None):
     if tag == 'html':
-        stri = "$this->closeHtml(FALSE);"
+        strings.append( "$this->closeHtml(FALSE);" )
+    elif tag == 'title':
+        stri =  "$this->Title( %s, FALSE );" % convertTuple(attr,data)
+        print("*********************************")
+        print(attr)
+        print(data)
+        print("*********************************")
         strings.append( stri )
-    if tag == 'title':
-        stri = "$this->Title( {%s}, FALSE );"
-        strings.append(stri )
-    if tag == 'span':
-        stri = "$this->closeSpan( {%s}, FALSE );"
+    elif tag == 'span':
+        stri = "$this->closeSpan( [], FALSE );"
         strings.append( stri )
-    if tag == 'head':
-        stri = "$this->closeHead( {%s}, FALSE );"
+    elif tag == 'head':
+        stri = "$this->closeHead( [], FALSE );"
         strings.append( stri )
-    if tag == 'body':
-        stri = "$this->closeBody( {%s}, FALSE );"
+    elif tag == 'body':
+        stri = "$this->closeBody( [], FALSE );"
         strings.append( stri  )
-    if tag == 'p':
-        stri = "$this->closeP( {%s}, FALSE );"
+    elif tag == 'p':
+        stri = "$this->closeP( [], FALSE );"
         strings.append( stri )
-    if tag == 'noscript':
-        stri = "$this->NoScript( {%s}, FALSE );"
+    elif tag == 'noscript':
+        stri = "$this->NoScript( [], FALSE );"
         strings.append( stri )
-    if tag == 'link':
-        stri = "$this->Link( {%s}, FALSE );"
+    elif tag == 'link':
+        stri = "$this->Link( [], FALSE );"
         strings.append(stri  )
-    if tag == 'button':
-        stri = "$this->Button( {%s}, FALSE );"
+    elif tag == 'button':
+        stri = "$this->Button( [], FALSE );"
         strings.append(stri )
-    if tag == 'iframe':
-        stri = "$this->iFrame( {%s}, FALSE );"
+    elif tag == 'iframe':
+        stri = "$this->iFrame( [], FALSE );"
         strings.append(stri )
-    if tag == 'meta':
-        str = "$this->Meta( {%s}, FALSE );"
+    elif tag == 'meta':
+        str = "$this->Meta( [], FALSE );"
         strings.append(stri )
-    if tag == 'script':
-        stri = "$this->Script( {%s}, FALSE );"
+    elif tag == 'script':
+        stri = "$this->Script( [], FALSE );"
         strings.append(stri )
-    if tag == 'div':
-        stri = "$this->closeDiv( {%s}, FALSE );"
+    elif tag == 'div':
+        stri = "$this->closeDiv( [], FALSE );"
         strings.append(stri )
-    if tag == 'label':
-        stri = "$this->Label( {%s}, FALSE );"
-        strings.append(stri )
-    if tag == 'input':
-        stri = "$this->Input( {%s}, FALSE );"
-        strings.append( stri)     
+    elif tag == 'input':
+        stri = "$this->Input( [], FALSE );"
+        strings.append( stri)
+    elif tag == 'form':
+        stri = "$this->closeForm( [], FALSE );"
+        strings.append( stri)
+    elif tag == 'label':
+        stri = ""
+    else:
+        strings.append("echo 'Error: no matching code for tag: %s'" % tag)
     
 
 class MyHtmlParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.Abucket = ""
+        self.Dbucket = ""
+        self.found = 0;
+    
     def handle_starttag(self, tag, attrs):
         print("Start tag: ", tag)
         for attr in attrs:
             print("    attr:", attr)
         openTag(tag,attrs)
+        self.Abucket = attrs
     def handle_endtag(self, tag):
-        print("End tag   :", tag)
-        closeTag(tag)
+        print("End tag   :", tag)        
+        if self.found == 1:
+            closeTag(tag,self.Abucket, self.Dbucket)
+            self.Abucket = ""
+            self.Dbucket = ""
+            self.found = 0;
+        if tag == 'html' or tag == 'body' or tag == 'form':
+            closeTag(tag)
+            
     def handle_data(self, data):
         print("Data     :", data)
+        print("length   :", len(data))
+        if len(data) != 1:
+            self.Dbucket = data
+            self.found = 1
     def handle_comment(self, data):
         print("Comment    :", data)
     def handle_entityref(self,name):
@@ -139,7 +193,12 @@ parser = MyHtmlParser()
 str = open('index.html', 'r').read()
 parser.feed(str)
 hdl = open('index.php', 'w')
+hdl.write('<?php\n')
+
 for line in strings:
     test = line
-    print(test)
+    hdl.write(test)
+    hdl.write("\n")
+hdl.write('?>')
 hdl.close()
+
