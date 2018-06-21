5b6acf2294045787217725df5f6bccd84ef11cd6
David Martin <dmartin@grovesindustrial.com>
2018-06-21 13:16:05 -0500

    added index files

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $this->openHtml(FALSE);
 $this->openHead( [], FALSE );
 $this->Meta( ['charset'=>"utf-8"], FALSE );
-$this->Title( [], FALSE );
+$this->Title( ['text'=>"Untitled Page"], FALSE );
 $this->Meta( ['name'=>"generator", 'content'=>"WYSIWYG Web Builder 14 Trial Version - http://www.wysiwygwebbuilder.com"], FALSE );
 $this->Link( ['href'=>"wb.validation.css", 'rel'=>"stylesheet"], FALSE );
 $this->Link( ['href'=>"contact_us.css", 'rel'=>"stylesheet"], FALSE );
@@ -15,22 +15,18 @@ $this->Script( ['src'=>"index.js"], FALSE );
 $this->Script( [], FALSE );
 $this->closeHead( [], FALSE );
 $this->openBody( [], FALSE );
-$this->openDiv( ['id'=>"DupontBanner", 'style'=>"height:176px; width:100%;margin-top:15px;margin-bottom:15px;"], FALSE );
-$this->aTag( ['href'=>"/searchresults.php?s=' . $sess_id . '&mfg=DuPont"], FALSE );
-$this->Image( ['class'=>"displayed2", 'src'=>"/shannon_images/banners/Dupont_Banner1.jpg", 'alt'=>""], FALSE );
-$this->closeaTag( ['class'=>'displayed2', 'src'=>'/shannon_images/banners/Dupont_Banner1.jpg', 'alt'=>'', 'text' =>'
-											'], FALSE );
+$this->openDiv( ['style'=>"height:176px; width:100%;margin-top:15px;margin-bottom:15px;"], FALSE );
+$this->openH1( [], FALSE );
+$this->openSpan( ['class'=>"searchTerm"], FALSE );
+$this->closeSpan( ['class'=>"searchTerm", 'text'=>"' . $searchValue . '"], FALSE );
+$this->closeH1( [], FALSE );
+$this->openH1( [], FALSE );
+echo 'Error: no matching code for tag: br'
+$this->aTag( ['href'=>"mailto:bstrine@grovesindustrial.com?subject=CenterPoint Product Help", 'style'=>"color: #0066CC;"], FALSE );
+$this->closeaTag( ['href'=>"mailto:bstrine@grovesindustrial.com?subject=CenterPoint Product Help", 'style'=>"color: #0066CC;", 'text'=>"E-mail your sales rep"], FALSE );
+echo 'Error: no matching code for tag: br'
+$this->closeH1( [], FALSE );
 $this->closeDiv( [], FALSE );
-$this->openP( ['style'=>"font-size:1.4em;padding-left:12px;"], FALSE );
-echo 'Error: no matching code for tag: strong'
-echo 'Error: no matching code for tag: strong'
-$this->closeP( [], FALSE );
-$this->openP( ['style'=>"line-height:normal; font-size:1.3em;margin-top:0px;padding-left:12px;"], FALSE );
-$this->closeP( ['style'=>'line-height:normal; font-size:1.3em;margin-top:0px;padding-left:12px;', 'text' =>'
-											Performing hazardous jobs comes with high levels of uncertainty. From volatile chemicals to
-											hostile enemies, the brave people who work in these high-stress fields rely on innovative personal
-											protective equipment solutions from DuPont.
-										'], FALSE );
 $this->closeBody( [], FALSE );
 $this->closeHtml(FALSE);
 ?>
