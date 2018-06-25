b550f26b6fa68b6d7621e044b014499e498f169f
David Martin <dmartin@grovesindustrial.com>
2018-06-25 10:59:31 -0500

    new indexes

--- index.php
+++ index.php
@@ -15,18 +15,7 @@ $this->Script( ['src'=>"index.js"], FALSE );
 $this->Script( [], FALSE );
 $this->closeHead( [], FALSE );
 $this->openBody( [], FALSE );
-$this->openDiv( ['style'=>"height:176px; width:100%;margin-top:15px;margin-bottom:15px;"], FALSE );
-$this->openH1( [], FALSE );
-$this->openSpan( ['class'=>"searchTerm"], FALSE );
-$this->closeSpan( ['class'=>"searchTerm", 'text'=>"' . $searchValue . '"], FALSE );
-$this->closeH1( [], FALSE );
-$this->openH1( [], FALSE );
-echo 'Error: no matching code for tag: br'
-$this->aTag( ['href'=>"mailto:bstrine@grovesindustrial.com?subject=CenterPoint Product Help", 'style'=>"color: #0066CC;"], FALSE );
-$this->closeaTag( ['href'=>"mailto:bstrine@grovesindustrial.com?subject=CenterPoint Product Help", 'style'=>"color: #0066CC;", 'text'=>"E-mail your sales rep"], FALSE );
-echo 'Error: no matching code for tag: br'
-$this->closeH1( [], FALSE );
-$this->closeDiv( [], FALSE );
+
 $this->closeBody( [], FALSE );
 $this->closeHtml(FALSE);
 ?>
